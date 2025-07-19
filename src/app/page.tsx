3697b34e6d6c7a553d0d297e63a85e34debd2966
guisilva10 <guisilva.070104@gmail.com>
2025-07-18 21:19:18 -0300

    chore: initial project

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex h-screen w-full items-center justify-center">
+      Hello world!
+    </main>
+  );
+}
+
+// lint-staged + husky
